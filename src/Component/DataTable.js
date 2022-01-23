50de8788ebd43447af3999c0d1c3224461211fe7
knechtel <maiquelknechtel@gmail.com>
2022-01-22 21:22:04 -0300

    Adicionando update-Equipment.

--- src/Component/DataTable.js
+++ src/Component/DataTable.js
@@ -12,42 +12,24 @@ function DataTable({ listAparelho }) {
   const onChangesChange = async (changes) => {
     var string = JSON.stringify(changes);
     if (string != "[]") {
-      console.log("olha isso  b ->  " + string);
       var key = null;
       var model = null;
       var serial = null;
       var marca = null;
       var obj = eval(string);
       if (string.includes("model")) {
-        console.log("ver aqui esse = key =  " + obj[0].data.model);
-        console.log("ver aqui esse = key =  " + obj[0].key);
         model = obj[0].data.model;
         key = obj[0].key;
       }
       if (string.includes("serial")) {
-        console.log("ver aqui esse = key =  " + obj[0].data.serial);
-        console.log("ver aqui esse = key =  " + obj[0].key);
         serial = obj[0].data.serial;
         key = obj[0].key;
       }
       if (string.includes("marca")) {
-        console.log("ver aqui esse = key =  " + obj[0].data.marca);
-        console.log("ver aqui esse = key =  " + obj[0].key);
         marca = obj[0].data.marca;
         key = obj[0].key;
       }
     }
-    //  console.log("olha isso   b[1][key] ->  " + b[1]["key"]);
-    // console.log("aquiii!  deu certo >>>model > = " + changes[0].data.model);
-    //console.log("aqui me interessa  -  ?>  " + changes[0].key);
-    console.log(
-      JSON.stringify({
-        id: key,
-        model: model,
-        serial: serial,
-        marca: marca,
-      })
-    );
     try {
       const response = fetch(`http://localhost:8080/update-equipment`, {
         method: "POST",
